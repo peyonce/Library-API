0704e616ac8956a33778783af0e7fbeaf8acc151
Your Name <you@example.com>
2025-09-10 17:00:07 +0200

    update code

--- Lib-api/src/index.ts
+++ Lib-api/src/index.ts
@@ -1,21 +1,23 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { logger } from './middleware/logger';
+import authorRoutes from './routes/authors'
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT: number = parseInt(process.env.PORT || '3000', 10);
 
 app.use(cors());
 app.use(express.json());
 app.use(logger);
+
+app.use('/authors', authorRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Library API is running' });
 });
 
 app.listen(PORT, () => {
-    console.log('server running at http://localhost:${PORT}');
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-
 
 
--- Lib-api/src/models/author.ts
+++ Lib-api/src/models/author.ts
@@ -12,7 +12,18 @@ export const authors: Author[] = [];
 
 
 export const createAuthor = (name: string, bio?: string, birthYear?: number): Author => {
-    const newAuthor: Author = { id: uuidv4(), name, bio, birthYear };
+    const newAuthor: Author = {
+        id: uuidv4(),
+        name,
+    };
+
+    if (bio !== undefined) {
+        newAuthor.bio = bio;
+
+        if (birthYear !== undefined) {
+            newAuthor.birthYear = birthYear;
+        }
+    }
     authors.push(newAuthor);
     return newAuthor;
 };
