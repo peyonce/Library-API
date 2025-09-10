dcaa871c86eced1d5ab0e9d55343bc59b002006d
Your Name <you@example.com>
2025-09-10 19:01:43 +0200

    update by id delete by id

--- Lib-api/src/routes/books.ts
+++ Lib-api/src/routes/books.ts
@@ -23,11 +23,43 @@ router.get('/', (req: Request, res: Response) => {
     res.json(books);
 });
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/:id', (req: Request, res: Response) => {
     const book: Book | undefined = books.find(b => b.id === req.params.id);
     if (!book) return res.status(404).json({ error: 'Book not found' });
 
     res.json(book);
 });
+
+router.post('/:id', (req: Request, res: Response) => {
+    const book: Book | undefined = books.find(b => b.id === req.params.id);
+    if (!book) return res.status(404).json({ error: 'Book not found' });
+
+    const { title, authorId, year, genre } = req.body;
+
+
+
+    if (authorId) {
+        const authorExists = authors.some(a => a.id === authorId);
+        if (!authorExists) return res.status(404).json({ error: 'Author not found' });
+        book.authorId = authorId;
+    }
+
+    if (title) book.title = title;
+    if (year !== undefined) book.year = Number(year)
+    if (genre !== undefined) book.genre = genre;
+
+    res.json(book);
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+    const index = books.findIndex(b => b.id === req.params.id);
+    if (index === -1) return res.status(404).json({ error: 'Book not found' });
+
+    const deleted: Book[] = books.splice(index, 1);
+    res.json({ message: 'Book deleted', book: deleted[0] });
+});
+
+
+
 
 
