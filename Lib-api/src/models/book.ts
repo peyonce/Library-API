b815339fdae313b3b151fdec0ed24b3a08e4b505
Your Name <you@example.com>
2025-09-10 17:27:23 +0200

    function to create new book

---
+++ Lib-api/src/models/book.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from 'uuid';
+import { authors } from './author';
+
+
+export interface Book {
+    id: string;
+    title: string;
+    authorId: string;
+    year?: number;
+    genre?: string;
+}
+
+export const books: Book[] = [];
+
+export const createBook = (title: string, authorId: string, year?: number, genre?: string): Book | null => {
+    const authorExists = authors.some(a => a.id === authorId)
+    if (!authorExists) return null;
+
+    const newBook: Book = {
+        id: uuidv4(),
+        title,
+        authorId,
+
+    };
+
+    if (year !== undefined) newBook.year = year;
+    if (genre !== undefined) newBook.genre = genre;
+
+    books.push(newBook);
+    return newBook;
+
+}
